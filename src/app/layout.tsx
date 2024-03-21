be4e3a1bf6e7498ad1ce9acdaf426cf345ccc425
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-03-21 15:16:44 +0100

    feat: Add styles to the Header components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
+import Footer from "./_components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Header />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+          {children}
+        <Footer />
+      </body>
     </html>
   );
 }
