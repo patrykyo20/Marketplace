6f5f54b5b96c5ef45fe81621d3e18e61ec5a7aaa
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-03-25 18:47:55 +0100

    feat: implement Products cart and get api

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -1,8 +1,35 @@
+'use client'
+import { UserButton, useUser } from '@clerk/nextjs';
+import { ShoppingCart } from 'lucide-react';
 import Image from 'next/image';
-import React from 'react'
+import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react'
+import CartContext from '../_context/CartContext';
+import GlobalApi from '../_utils/GlobalApi';
+import Cart from './Cart';
 
 function Header() {
-  return (
+  const {user} = useUser();
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [openCart, setOpenCart] = useState<boolean>(false);
+  const { cart, setCart } = useContext(CartContext);
+
+  const getCartItems = useCallback(() => {
+    GlobalApi.getUserCart(user?.primaryEmailAddress?.emailAddress).then(response => {
+      setCart(response.data.data);
+    });
+  }, [setCart, user?.primaryEmailAddress?.emailAddress]);
+
+  useEffect(() => {
+    if (window.location.href.includes('sign-up') || window.location.href.includes('sign-in')) {
+      setIsLogin(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    getCartItems();
+  }, [getCartItems, user]);
+
+  return !isLogin && (
     <header className="bg-white">
       <div className="mx-auto flex h-16 items-center gap-8 px-4 sm:px-6 lg:px-8 shadow-sm">
         <Image
@@ -40,21 +67,32 @@ function Header() {
           </nav>
 
           <div className="flex items-center gap-4">
+           {!user ?
             <div className="sm:flex sm:gap-4">
-              <a
-                className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-600"
-                href="#"
-              >
-                Login
-              </a>
+                <a
+                  className="block rounded-md bg-primary px-5 py-2.5 text-sm font-medium text-white transition hover:bg-blue-600"
+                  href="/sing-in"
+                >
+                  Login
+                </a>
 
-              <a
-                className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-primary transition hover:text-blue-600 sm:block"
-                href="#"
-              >
-                Register
-              </a>
-            </div>
+                <a
+                  className="hidden rounded-md bg-gray-100 px-5 py-2.5 text-sm font-medium text-primary transition hover:text-blue-600 sm:block"
+                  href="/sing-up"
+                >
+                  Register
+                </a>
+              </div>
+              :
+              <div className='flex items-center gap-5'>
+                <div className='flex gap-2' onClick={() => setOpenCart(!openCart)}>
+                  <ShoppingCart className='cursor-pointer'/> {`(${cart?.length})`}
+                </div>
+                <UserButton />
+              </div>
+            }
+
+            {openCart && <Cart />}
 
             <button
               className="block rounded bg-gray-100 p-2.5 text-gray-600 transition hover:text-gray-600/75 md:hidden"
