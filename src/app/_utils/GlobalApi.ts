86d9d2261cfd378772cde86ce2790c125667f838
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-03-25 18:53:21 +0100

    feat: Add get and delete cart products

--- src/app/_utils/GlobalApi.ts
+++ src/app/_utils/GlobalApi.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Product from "../_types/Product";
 
 const apiKey = process.env.NEXT_PUBLIC_REST_API_KEY;
 const apiUrl = 'http://localhost:1337/api';
@@ -11,9 +12,21 @@ const axiosClient = axios.create({
 })
 
 const getLatestProducts = () => axiosClient.get('/products?populate=*')
+const getProductById = (id: string) => axiosClient.get(`/products/${id}?populate=*`)
 
+const getProductByCategory = (category: string | undefined) => axiosClient.get(`/products?filters[category][$eq]=${category}&populate=*`)
 
+const postProductToCart = (data: any) => axiosClient.post('/carts', data)
+
+const getUserCart = (email: string | undefined) => axiosClient.get(`/carts?populate[products][populate][0]=banner&filters[email][$eq]=${email}`)
+
+const deleteUserCart = (productId: number) => axiosClient.delete(`/carts/${productId}`)
 // eslint-disable-next-line import/no-anonymous-default-export
 export default {
   getLatestProducts,
+  getProductById,
+  getProductByCategory,
+  postProductToCart,
+  getUserCart,
+  deleteUserCart,
 }
